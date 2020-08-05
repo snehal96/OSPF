4fbbe1467276a6f4c2d9c6a06e65e07cd20a5a28
snehal <snehal.maheshwari14@gmail.com>
2020-08-05 23:34:43 +0530

    updated contact form to work on netlify

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -5,6 +5,7 @@ import {
   FormGroup,
   FormBuilder,
 } from "@angular/forms";
+import { HttpParams, HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-contact",
@@ -12,9 +13,9 @@ import {
   styleUrls: ["./contact.component.scss"],
 })
 export class ContactComponent implements OnInit {
-  form: FormGroup;
-  constructor(public fb: FormBuilder) {
-    this.form = this.fb.group({
+  contactForm: FormGroup;
+  constructor(public fb: FormBuilder, private http: HttpClient) {
+    this.contactForm = this.fb.group({
       name: "",
       feedback: "",
       email: "",
@@ -33,5 +34,41 @@ export class ContactComponent implements OnInit {
     }
 
     return this.email.hasError("email") ? "Not a valid email" : "";
+  }
+
+  submitForm() {
+    const body = new HttpParams()
+      .set("form-name", "contact")
+      .append("name", this.contactForm.value.name)
+      .append("feedback", this.contactForm.value.feedback)
+      .append("email", this.contactForm.value.email)
+      .append("Telegram id", this.contactForm.value.tid)
+      .append("contact", this.contactForm.value.contact);
+
+    this.http
+      .post("/", body.toString(), {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      })
+      .subscribe(
+        (res) => {},
+        (err) => {
+          if (err instanceof ErrorEvent) {
+            //client side error
+            alert("Something went wrong when sending your message.");
+            console.log(err.error.message);
+          } else {
+            //backend error. If status is 200, then the message successfully sent
+            if (err.status === 200) {
+              alert("Your message has been sent!");
+            } else {
+              alert("Something went wrong when sending your message.");
+              console.log("Error status:");
+              console.log(err.status);
+              console.log("Error body:");
+              console.log(err.error);
+            }
+          }
+        }
+      );
   }
 }
